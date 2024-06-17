99f09c5915a0a63edc21043b821f79bbbda17903
nathanmintegui <nathanberger.rs@hotmail.com>
2024-06-16 21:11:20 -0300

    add user route

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -9,29 +9,33 @@ import (
 
 	"github.com/nathanmintegui/edina/internal/database"
 	"github.com/nathanmintegui/edina/internal/http"
+	"github.com/nathanmintegui/edina/internal/user"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-  ctx := context.Background()
+	ctx := context.Background()
 
-   err := godotenv.Load(".env")
+	err := godotenv.Load(".env")
 
-   if err != nil{
-    log.Fatalf("Error loading .env file: %s", err)
-   }
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
 
-  connectionString := os.Getenv("POSTGRES_URI")
-  conn,err := database.NewConnection(ctx, connectionString)
-  if(err != nil) {
-    panic(err)
-  }
+	connectionString := os.Getenv("POSTGRES_URI")
+	conn, err := database.NewConnection(ctx, connectionString)
+	if err != nil {
+		panic(err)
+	}
 
-  defer conn.Close()
+	defer conn.Close()
 
-  r := gin.Default()
-  http.SetRoutes(r)
+	r := gin.Default()
+	http.SetRoutes(r)
 
-  r.Run()
+	user.Configure()
+	user.SetRoutes(r)
+
+	r.Run()
 }
