46cba3f57fb13681fb7691c57699ac5da3c34b93
nathanmintegui <nathanberger.rs@hotmail.com>
2024-06-18 19:48:38 -0300

    chore: configure login route and import component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import type { Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "login",
+		title: "Login Screen",
+		component: LoginComponent,
+	},
+];
